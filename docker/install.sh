1ceb46a4dcc613e9f8ce26ef67e585559a8d3cf4
Daniel Dunn <dannydunn@example.com>
2026-09-12 19:28:29 -0600

    Persistent docker home

--- docker/install.sh
+++ docker/install.sh
@@ -5,7 +5,12 @@
 echo "Installing kaithem-container-host-services"
 bash ./kaithem-container-host-services/install.sh
 
+# Very important not to let docker make these
+# as root!!
 mkdir -p ~/kaithem-docker-home
+mkdir -p ~/kaithem-docker-home/kaithem
+mkdir -p ~/kaithem-docker-home/matter
+
 
 #If file doesn't exist, create it
 if [ ! -f $HOME/kaithem-docker-home/docker-compose.yml ]; then
@@ -19,4 +24,4 @@ bash ./compose-groups-override.sh
 cd $HOME/kaithem-docker-home
 
 echo "Starting service"
-KAITHEM_GROUPS=$(getent group audio video dialout rtkit gpio i2c spi render bluetooth serial | cut -d: -f3 | tr '\n' ' ') KAITHEM_UID=$(id -u) KAITHEM_GROUP=$(id -g) KAITHEM_USER=$(id -un) sudo -E docker compose up -d kaithem
+KAITHEM_GROUPS=$(getent group audio video dialout rtkit gpio i2c spi render bluetooth serial | cut -d: -f3 | tr '\n' ' ') KAITHEM_UID=$(id -u) KAITHEM_GROUP=$(id -g) KAITHEM_USER=$(id -un) sudo -E docker compose up --remove-orphans -d kaithem matter
