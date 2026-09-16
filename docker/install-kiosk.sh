0165e58d9730e2e3190507054788a4015f5d00b1
Daniel Dunn <dannydunn@example.com>
2026-09-15 18:14:54 -0600

    entrypoint

--- docker/install-kiosk.sh
+++ docker/install-kiosk.sh
@@ -88,4 +88,4 @@ Terminal=false
 EOF
 
 cd ~/kaithem-docker-home
-KAITHEM_GROUPS=$(getent group audio video dialout rtkit gpio i2c spi render bluetooth serial | cut -d: -f3 | tr '\n' ' ') KAITHEM_UID=$(id -u) KAITHEM_GROUP=$(id -g) KAITHEM_USER=$(id -un) sudo -E docker compose run --remove-orphans --entrypoint bash kiosk
+KAITHEM_GROUPS=$(getent group audio video dialout rtkit gpio i2c spi render bluetooth serial | cut -d: -f3 | tr '\n' ' ') KAITHEM_UID=$(id -u) KAITHEM_GROUP=$(id -g) KAITHEM_USER=$(id -un) sudo -E docker compose run --remove-orphans kiosk
